af643c20a9bec01481f946ae391f7574053daa45
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2026-01-06 12:53:20 -0300

    Update emr.tf

--- IaC/emr/emr.tf
+++ IaC/emr/emr.tf
@@ -45,6 +45,7 @@ variable "core_instance_ebs_volume_size" { default = "80" }
 variable "security_configuration_name" { default = null }
 
 # Definição da variável 'log_uri' com valor padrão (substitua account-id pelo id da sua conta)
+# variable "log_uri" { default = "s3://dsa-projeto1-<account-id>" }
 variable "log_uri" { default = "s3://dsa-projeto1-<account-id>" }
 
 # Definição da variável 'configurations' com valor padrão nulo
